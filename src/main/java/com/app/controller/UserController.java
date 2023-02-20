7846d8958e04748e2a133f0600f0f402fb7f6f7c
gcad14 <2011766189@qq.com>
2023-02-20 13:18:49 +0800

    delete swagger

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -5,8 +5,6 @@ import com.app.service.ExportService;
 import com.app.service.SendMailService;
 import com.app.service.UserService;
 import fr.opensagres.xdocreport.core.XDocReportException;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
-@Api(tags = "用户管理")
 @RequestMapping("/user")
 @CrossOrigin
 public class UserController {
@@ -30,21 +27,18 @@ public class UserController {
     private SendMailService sendMailService;
 
     @GetMapping("/getUserInfo")
-    @ApiOperation("获取用户信息")
     public String getUser() {
         return userService.getUser();
     }
 
     //导出Word
     @GetMapping("/export/word")
-    @ApiOperation("导出Word")
     public void exportWord(HttpServletResponse response) throws IOException, XDocReportException {
         exportService.word(response);
     }
 
     //发送邮件
     @GetMapping("/send/email")
-    @ApiOperation("发送邮件")
     public void email(MailRequest mailRequest) {
         sendMailService.sendSimpleMail(mailRequest);
     }
